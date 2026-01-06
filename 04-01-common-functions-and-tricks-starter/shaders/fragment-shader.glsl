d7f0962d0036733afc8d618bb7848bd3302b1631
Jonathan Tyler Weber <jonathantweber@gmail.com>
2026-01-06 14:55:00 -0600

    clamp

--- 04-01-common-functions-and-tricks-starter/shaders/fragment-shader.glsl
+++ 04-01-common-functions-and-tricks-starter/shaders/fragment-shader.glsl
@@ -32,7 +32,16 @@ void main() {
 
   float value1 = vUvs.x;
   float value2 = smoothstep(0.0, 1.0, vUvs.x);
-  float value3 = step(0.5, vUvs.x);
+  // float value3 = step(0.5, vUvs.x);
+
+  // min
+  // float value3 = min(vUvs.x, 0.4);
+  // max
+  // float value3 = max(vUvs.x, 0.6);
+  // clamp
+  // float value3 = clamp(vUvs.x, 0.25, 0.75);
+  // diy clamp
+  float value3 = min(max(vUvs.x, 0.25), 0.75);
 
   float line1 = smoothstep(0.0, 0.005, abs(vUvs.y - 0.333));
   float line2 = smoothstep(0.0, 0.005, abs(vUvs.y - 0.666));
@@ -42,11 +51,11 @@ void main() {
   float stepLine = smoothstep(0.0, 0.0075, abs(vUvs.y - mix(0.0, 0.333, value3)));
 
   if (vUvs.y > 0.666) {
-    color = mix(red, blue, vUvs.x);
+    color = mix(red, blue, value1);
   } else if (vUvs.y > 0.333 && vUvs.y < 0.666) {
-    color = mix(red, blue, smoothstep(0.0, 1.0, vUvs.x));
+    color = mix(red, blue, value2);
   } else {
-    color = mix(red, blue, step(0.5, vUvs.x));
+    color = mix(red, blue, value3);
   }
 
   color = mix(white, color, line1);
